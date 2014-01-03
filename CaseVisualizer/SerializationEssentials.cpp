81bd53f049a373c39f48f14da8ff29d78d9842a7
MHesham <mhesham.fcis@gmail.com>
2014-01-03 01:45:29 -0800

    Update the SerializationEssentials.cpp with new serialize class names and remove obsolete serializable classes
    Signed-off-by: MHesham <mhesham.fcis@gmail.com>

--- CaseVisualizer/SerializationEssentials.cpp
+++ CaseVisualizer/SerializationEssentials.cpp
@@ -6,10 +6,10 @@
 #include "ObjectFactory.h"
 #endif
 #ifndef OBJECTFORMATTER_H
-#include "ObjectFormatter.h"
+	#include "ObjectFormatter.h"
 #endif
 #ifndef OBJECTSERIALIZER_H
-#include "ObjectSerializer.h"
+	#include "ObjectSerializer.h"
 #endif
 
 #ifndef CASEBASEEX_H
@@ -38,9 +38,6 @@
 #endif
 #ifndef ATTACKGROUNDACTION_H
 #include "AttackGroundAction.h"
-#endif
-#ifndef BUILDACTION_H
-#include "BuildAction.h"
 #endif
 #ifndef BUILDACTIONEX_H
 #include "BuildActionEx.h"
@@ -77,51 +74,59 @@
 #endif
 
 #ifndef PLANGRAPH_H
-#include "PlanGraph.h"
+	#include "PlanGraph.h"
 #endif
+
+using namespace IStrategizer;
 
 void SerializationEssentials::Init()
 {
-	g_ObjectFactory.AddPrototype(new CaseEx);
-	g_ObjectFactory.AddPrototype(new GameStateEx);
+	static bool initialized = false;
 
-	g_ObjectFactory.AddPrototype(new Colony);
+	if (initialized)
+		return;
 
-	g_ObjectFactory.AddPrototype(new AttackEnemyGoal);
-	g_ObjectFactory.AddPrototype(new BuildBaseGoal);
-	g_ObjectFactory.AddPrototype(new WinWargusGoalEx);
-	g_ObjectFactory.AddPrototype(new TrainForceGoal);
+    g_ObjectFactory.AddPrototype(new CaseEx);
+    g_ObjectFactory.AddPrototype(new GameStateEx);
 
-	g_ObjectFactory.AddPrototype(new AttackEntityAction);
-	g_ObjectFactory.AddPrototype(new AttackGroundAction);
-	g_ObjectFactory.AddPrototype(new BuildAction);
+    g_ObjectFactory.AddPrototype(new Colony);
+
+    g_ObjectFactory.AddPrototype(new AttackEnemyGoal);
+    g_ObjectFactory.AddPrototype(new BuildBaseGoal);
+    g_ObjectFactory.AddPrototype(new WinWargusGoalEx);
+    g_ObjectFactory.AddPrototype(new TrainForceGoal);
+
+    g_ObjectFactory.AddPrototype(new AttackEntityAction);
+    g_ObjectFactory.AddPrototype(new AttackGroundAction);
+    g_ObjectFactory.AddPrototype(new ResearchAction);
+    g_ObjectFactory.AddPrototype(new TrainAction);
 	g_ObjectFactory.AddPrototype(new BuildActionEx);
-	g_ObjectFactory.AddPrototype(new ResearchAction);
-	g_ObjectFactory.AddPrototype(new TrainAction);
 
-	g_ObjectFactory.AddPrototype(new ResourceExist);
-	g_ObjectFactory.AddPrototype(new ResearchDone);
-	g_ObjectFactory.AddPrototype(new EntityObjectExist);
-	g_ObjectFactory.AddPrototype(new EntityClassExist);
-	g_ObjectFactory.AddPrototype(new CheckPositionFilterCount);
-	g_ObjectFactory.AddPrototype(new CheckEntityObjectSimilarity);
-	g_ObjectFactory.AddPrototype(new CheckEntityObjectAttribute);
-	g_ObjectFactory.AddPrototype(new CheckEntityClassAttribute);
-	g_ObjectFactory.AddPrototype(new CheckColonyFilterCount);
-	g_ObjectFactory.AddPrototype(new And);
-	g_ObjectFactory.AddPrototype(new Not);
-	g_ObjectFactory.AddPrototype(new Or);
+    g_ObjectFactory.AddPrototype(new ResourceExist);
+    g_ObjectFactory.AddPrototype(new ResearchDone);
+    g_ObjectFactory.AddPrototype(new EntityObjectExist);
+    g_ObjectFactory.AddPrototype(new EntityClassExist);
+    g_ObjectFactory.AddPrototype(new CheckPositionFilterCount);
+    g_ObjectFactory.AddPrototype(new CheckEntityObjectSimilarity);
+    g_ObjectFactory.AddPrototype(new CheckEntityObjectAttribute);
+    g_ObjectFactory.AddPrototype(new CheckEntityClassAttribute);
+    g_ObjectFactory.AddPrototype(new CheckColonyFilterCount);
+    g_ObjectFactory.AddPrototype(new And);
+    g_ObjectFactory.AddPrototype(new Not);
+    g_ObjectFactory.AddPrototype(new Or);
 
-	g_ObjectFactory.AddPrototype(new PlanGraph);
-	g_ObjectFactory.AddPrototype(new Diagraph<NodeValue, EdgeAnnotation>, "Diagraph(PlanStepEx*,vector(Expression*))");
-	g_ObjectFactory.AddPrototype(new GraphNode<NodeValue, EdgeAnnotation>, "GraphNode(PlanStepEx*,vector(Expression*))");
-	g_ObjectFactory.AddPrototype(new GraphEdge<EdgeAnnotation>, "GraphEdge(vector(Expression*))");
-
-	g_ObjectFactory.AddPrototype(new CaseBaseEx);
+    g_ObjectFactory.AddPrototype(new PlanGraph);
+    g_ObjectFactory.AddPrototype(new Diagraph<NodeValue, EdgeAnnotation>, "Diagraph(PlanStepEx*,vector(Expression*))");
+    g_ObjectFactory.AddPrototype(new GraphNode<NodeValue, EdgeAnnotation>, "GraphNode(PlanStepEx*,vector(Expression*))");
+    g_ObjectFactory.AddPrototype(new GraphEdge<EdgeAnnotation>, "GraphEdge(vector(Expression*))");
+    
+    g_ObjectFactory.AddPrototype(new CaseBaseEx);
 
 	g_ObjectFactory.AddPrototype(new CellFeature);
 	g_ObjectFactory.AddPrototype(new ForceDescription);
 	g_ObjectFactory.AddPrototype(new BuildingDescription);
 
-	g_ObjectFormatter.FinalizeTypeTable(g_ObjectSerializer.TypeTable(), g_ObjectFactory.GetObjectTable());
+    g_ObjectFormatter.FinalizeTypeTable(g_ObjectSerializer.TypeTable(), g_ObjectFactory.GetObjectTable());
+	
+	initialized = true;
 }
