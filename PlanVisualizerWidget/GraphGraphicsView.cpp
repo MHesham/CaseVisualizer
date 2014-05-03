c56af45de0f3a0fe396d2c16b295fe88528c4688
MHesham <mhesham.fcis@gmail.com>
2014-05-03 11:02:56 -0700

    Fix the bug of NodeEdges position is not updated on moving a node
    Signed-off-by: MHesham <mhesham.fcis@gmail.com>

--- PlanVisualizerWidget/GraphGraphicsView.cpp
+++ PlanVisualizerWidget/GraphGraphicsView.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "IStrategizerEx.h"
 #include "GraphGraphicsView.h"
+#include <QGLWidget>
 
 using namespace IStrategizer;
 using namespace std;
@@ -12,9 +13,9 @@ GraphGraphicsView::GraphGraphicsView()
     this->setDragMode(QGraphicsView::DragMode::ScrollHandDrag);
     this->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
 
-    this->setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
+    this->setRenderHints(QPainter::Antialiasing);
     this->setOptimizationFlags(QGraphicsView::DontSavePainterState);
-    this->setViewportUpdateMode(QGraphicsView::SmartViewportUpdate);
+    this->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
 }
 //----------------------------------------------------------------------------------------------
 void GraphGraphicsView::wheelEvent(QWheelEvent* p_wheelEvent)
